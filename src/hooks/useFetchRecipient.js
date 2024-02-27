9e2c96a7b12e18e24c47748373f11ea086b00c14
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-02-27 23:09:06 +0700

    tạo giao diện lít chat và custom 1 hook lấy thông tin người nhận message

---
+++ src/hooks/useFetchRecipient.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { baseUrl, getRequest } from "../utils/services";
+
+export const useFetchRecipientUser = (chat, user) => {
+    const [recipientUser, setRecipientUser] = useState(null);
+
+    const [error, setError] = useState(null);
+
+    const recipientId = chat?.members.find((id) => id !== user?._id);
+
+    useEffect(() => {
+        const getUser = async () => {
+            if (!recipientId) return null;
+
+            // gọi api tìm người nhận
+            const response = await getRequest(
+                `${baseUrl}/users/find/${recipientId}`
+            );
+
+            if (response.error) {
+                return setError(response?.message);
+            }
+
+            setRecipientUser(response);
+        };
+
+        getUser();
+    }, []);
+
+    return { recipientUser };
+};
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,12 +1,37 @@
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
+import { Container, Stack } from "react-bootstrap";
+import UserChats from "../components/chat/UserChat";
+import { AuthContext } from "../context/AuthContext";
 
 const Chat = () => {
+    const { user } = useContext(AuthContext);
     const { userChats, isUserChatsLoading, userChatsError } =
         useContext(ChatContext);
 
-    console.log("UserChats: ", userChats);
-    return <>Chat</>;
+    return (
+        <Container>
+            {userChats?.length < 1 ? null : (
+                <Stack
+                    direction="horizontal"
+                    gap={4}
+                    className="align-items-start"
+                >
+                    <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+                        {isUserChatsLoading && <p>Loading chats...</p>}
+                        {userChats?.map((chat, index) => {
+                            return (
+                                <div key={`userChat-${index}`}>
+                                    <UserChats chat={chat} user={user} />
+                                </div>
+                            );
+                        })}
+                    </Stack>
+                    <p>ChatBox</p>
+                </Stack>
+            )}
+        </Container>
+    );
 };
 
 export default Chat;
